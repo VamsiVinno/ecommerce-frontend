6911d719d8dab06aa8d8d79639e22b9b2e3d5c12
Jithin M V <jithin@vinnovatelabz.com>
2021-12-10 14:35:19 +0530

    latest changes

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 80;
+
 // Serve only the static files form the dist directory
-app.use(express.static("./dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", {
-    root: "dist",
-  })
+  res.sendFile(path.join(__dirname, "dist/en-US/index.html"))
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4300);
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+});
